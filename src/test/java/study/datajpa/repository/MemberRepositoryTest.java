5fb2770638ba9d790942dc1b1e7bb470f477dadd
danbi <leedanbiii@naver.com>
2023-11-15 02:35:20 +0900

    벌크성 수정 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -157,6 +157,21 @@ class MemberRepositoryTest {
         assertThat(page.getTotalPages()).isEqualTo(2);
         assertThat(page.isFirst()).isTrue(); // 첫번째 페이지야?
         assertThat(page.hasNext()).isTrue(); // 다음 페이지 있어?
+    }
 
+    @Test
+    public void bulkUpdate() {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 29));
+        memberRepository.save(new Member("member5", 40));
+
+        // when
+        int resultcount = memberRepository.bulkAgePlus(20);
+
+        // then
+        assertThat(resultcount).isEqualTo(3);
     }
 }
